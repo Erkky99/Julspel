6268c44bbcf7351080769c5270da9a2af9693d1d
ErikBrink <ErikBrink@10.60.248.190>
2017-01-11 10:17:21 +0100

    Frame start.

---
+++ src/main/java/eu/tankernn/julklapp/Julspel.java
@@ -0,0 +1,41 @@
+package eu.tankernn.julklapp;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Julspel {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Julspel window = new Julspel();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Julspel() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
